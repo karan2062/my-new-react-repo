9c9f8045cb9c93a780a9705258c9a1b78089bad2
karan2062 <karanchaudhary2062@gmail.com>
2025-05-06 13:19:36 +0545

    Add Navbar and footer component

---
+++ Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+    return (
+        <footer style={{ backgroundColor: "black", color: "white", padding: "10px", textAlign: "center", marginTop: "20px" }}>
+            <p>&copy; 2025 My Website. All rights reserved.</p>
+        </footer>
+    );
+};
